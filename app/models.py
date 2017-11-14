b611a64e604478d99cb7ba0c0317a04b7396544a
Freddie Vargus <fvargus4@bu.edu>
2017-11-14 18:44:23 -0500

    MAINT: Change restaurant migrations

--- app/models.py
+++ app/models.py
@@ -3,17 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Restaurants(models.Model):
-    restaurantName = models.CharField(max_length=50)
-    restaurantLocation = models.CharField(max_length=50)
-    foodType = models.CharField(max_length=50)
+    r_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    food_type = models.CharField(max_length=50)
+    image_url = models.URLField()
+    yelp_url = models.URLField()
+    phone = models.CharField(max_length=50)
+    rating = models.FloatField()
+    category_one = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.restaurantName
+        return (self.name, self.location)
 
 
 class UserRequest(models.Model):
-    location = models.CharField(max_length=15)
-    term = models.CharField(max_length=15)
+    location = models.CharField(max_length=30)
+    term = models.CharField(max_length=30)
 
     def __str__(self):
         return (self.location, self.term)
